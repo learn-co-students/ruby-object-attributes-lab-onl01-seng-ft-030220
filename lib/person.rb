e8bbafba26832eb0c1c624c4eb26db535f89d4f4
Amal Abdu <abduamal@ymail.com>
2020-03-15 02:40:13 +0000

    Done.

---
+++ lib/person.rb
@@ -0,0 +1,17 @@
+class Person
+  def name=(name_of_person)
+    @name = name_of_person
+  end
+  
+  def name
+    @name
+  end
+  
+  def job=(job_name)
+    @job = job_name
+  end
+  
+  def job
+    @job
+  end
+end
